cf3c53adf25b570b3cd4d2c5eaaa1942d6441399
Vincente <vinmanneal@gmail.com>
2025-07-21 15:48:40 -0500

    Added score text and began adding scare counter to game manager

--- Assets/Scripts/Unit5Scripts/GameManager.cs
+++ Assets/Scripts/Unit5Scripts/GameManager.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
-
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshPro scoreText;
 
+    private int score;
     private float spawnRate = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        score = 0;
         StartCoroutine(SpawnTarget());
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
